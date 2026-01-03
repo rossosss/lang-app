42db386d9cce1cf1d833625e71fe3db40fc88dc4
rossosss <daniilatchekin@yandex.ru>
2026-01-03 20:34:19 +0300

    feat: add auth
    Add auth and marketing page

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton, } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+
 export const Header = () => {
 	return (
 		<header className="h-20 w-full border-b-2 border-slate-200 px-4">
-			Header
+			<div className="lg:max-w-5xl mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.png" height={40} width={40} alt="Mascot"/>
+          <h1 className="text-2xl font-extrabold text-neutral-500 tracking-wide">
+            Lang
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton 
+              mode="modal"
+              forceRedirectUrl="/learn"
+              signUpForceRedirectUrl="learn"
+            >
+              <Button size="lg" variant="ghost">
+                Логин
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
 		</header>
 	);
 };
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,53 @@
 import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Button size="lg" variant="destructive">Lang</Button>
+    <div className="max-w-247 mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center pd-4 gap-2">
+      <div className="relative w-60 h-60 lg:w-106 lg:h-106 mb-8 lg:mb-0">
+        <Image src="/hero.png" fill alt="Hero"/>
+      </div>
+      <div className="flex flex-col items-center gap-y-8 ">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-120 text-center">Изучай, практикуй и совершенствуй новые языки вместе с Lang</h1>
+      
+        <div>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                forceRedirectUrl="/learn"
+                signInForceRedirectUrl="/learn"
+              >
+                <Button size="lg" variant="secondary" className="w-full">
+                  Начать
+                </Button>
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                forceRedirectUrl="/learn"
+                signUpForceRedirectUrl="/learn"
+              >
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  У меня уже есть аккаунт
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">
+                  Продолжить
+                </Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   )
 }
