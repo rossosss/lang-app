939f886804bf6a6c20eab6278c20e6a19a0be5f7
rossosss <166108354+rossosss@users.noreply.github.com>
2026-01-04 16:13:34 +0300

    feat: add footer
    Create Footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
 export const Footer = () => {
   return (
-    <footer className="hidden lg:block h-20 w-20 border-t-2 border-slate-200 p-2">
-      Footer
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+      <div className="max-w-5xl mx-auto flex items-center justify-center h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image src="/gb.svg" alt="Great Britain" height={32} width={40} className="mr-4 rounded-md"/>
+          Английский
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image src="/es.svg" alt="Spain" height={32} width={40} className="mr-4 rounded-md"/>
+          Испанский
+        </Button>
+      </div>
     </footer>
   )
 }
